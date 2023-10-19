8ed9698454ec70f9250f206b14b022b8a11b8765
Ariel Silahian <ariel.silahian@gmail.com>
2023-10-19 19:01:27 -0400

    chapter 4 code

--- chapter_4/RMS.cpp
+++ chapter_4/RMS.cpp
@@ -1,3 +1,32 @@
+
+/*
+----------------------------------------------------------------------------------
+DISCLAIMER:
+
+The following code is primarily for illustrative purposes, aiming to showcase the 
+concept of dynamic portfolio optimization using Reinforcement Learning in C++ with TensorFlow. 
+It is not intended for actual trading or any real-world financial decision-making. While 
+efforts have been made to ensure its accuracy, there's no guarantee that it will compile or 
+function flawlessly in all environments.
+
+This code includes:
+1. Structures and classes representing market data and financial instruments.
+2. A representation of a trading environment where an agent can buy, sell, or hold assets.
+3. A basic implementation of a Deep Q-Network (DQN) agent that learns to optimize a portfolio 
+   based on Reinforcement Learning.
+4. A main routine that ties all the components together, simulating the agent's interactions 
+   with the trading environment over multiple episodes.
+
+Financial markets are complex, influenced by countless factors. The model in this code is a 
+simplified representation and might not account for all real-world intricacies. It's crucial 
+to consult with financial experts before making any investment decisions and to thoroughly 
+test any models in safe environments before real-world application.
+
+Use this code at your own risk.
+----------------------------------------------------------------------------------
+*/
+
+
 #include <vector>
 #include <string>
 #include <map>
@@ -31,10 +60,15 @@ public:
     MarketData(const std::map<std::string, std::vector<DataPoint>>& inputData)
         : data(inputData), currentPosition(0) {}
 
-    // Move to the next data point
+    // Assuming all tickers have the same length of data for simplicity
     void next() {
         if (currentPosition < data.begin()->second.size() - 1) {
             ++currentPosition;
+            for (auto &[ticker, dataPoints] : data) {
+                // Assuming all tickers have the same data length for now
+                // This will move all ticker data positions forward
+                ++currentPosition;
+            }
         }
     }
 
@@ -52,36 +86,30 @@ public:
     void reset() {
         currentPosition = 0;
     }
+    
+    // For demonstration purposes, return the last 'days' data points for the ticker
+    std::vector<DataPoint> getHistoricalData(const std::string& ticker, size_t days) const {
+        if (data.at(ticker).size() < days) {
+            return data.at(ticker);
+        } else {
+            return std::vector<DataPoint>(data.at(ticker).end() - days, data.at(ticker).end());
+        }
+    }
 
-    // ... additional methods as necessary
-    std::vector<DataPoint> getHistoricalData(const std::string& ticker, size_t days) const;
-    void setDataRange(size_t start, size_t end);
-    DataPoint aggregateData(const std::string& ticker, size_t days) const;
-    double calculateMovingAverage(const std::string& ticker, size_t window) const;
-    void adjustForSplitsAndDividends(const std::string& ticker);
-    bool validateDataConsistency(const std::string& ticker) const;
-    bool isDataSynchronized() const;
-    std::vector<std::string> getEvents(const std::string& ticker, const DataPoint& point) const;
 };
 
 
-class Portfolio {
-private:
-    // Data structure to hold the assets and their quantities.
-    std::map<std::string, double> assets; // Key: Ticker symbol, Value: Quantity
-
-    // Cash balance in the portfolio.
-    double cashBalance;
-
+class Portfolio { // Data structure to hold the assets and their quantities.
+private:    
+    std::map<std::string, double> assets; // Key: Ticker symbol, Value: Quantity    
+    double cashBalance; // Cash balance in the portfolio.
 public:
     // Constructor to initialize the portfolio with a starting cash balance.
     Portfolio(double initialCash) : cashBalance(initialCash) {}
-
     // Method to add an asset to the portfolio.
     void addAsset(const std::string& ticker, double quantity) {
         assets[ticker] += quantity;
     }
-
     // Method to remove an asset from the portfolio.
     void removeAsset(const std::string& ticker, double quantity) {
         if (assets[ticker] < quantity) {
@@ -97,22 +125,37 @@ public:
         }
         return 0.0;
     }
-
     // Method to adjust the cash balance when buying/selling assets.
     void adjustCashBalance(double amount) {
         cashBalance += amount;
     }
-
     // Method to get the current cash balance.
     double getCashBalance() const {
         return cashBalance;
     }
-
+    void buy(const std::string& ticker, double quantity, const MarketData& marketData) {
+        double cost = marketData.getCurrentData(ticker).close * quantity;
+        if (cashBalance < cost) {
+            throw std::runtime_error("Not enough cash to buy.");
+        }
+        addAsset(ticker, quantity);
+        adjustCashBalance(-cost);
+    }
+    void sell(const std::string& ticker, double quantity, const MarketData& marketData) {
+        double earnings = marketData.getCurrentData(ticker).close * quantity;
+        if (getAssetQuantity(ticker) < quantity) {
+            throw std::runtime_error("Not enough assets to sell.");
+        }
+        removeAsset(ticker, quantity);
+        adjustCashBalance(earnings);
+    }
     // Method to compute the portfolio's total value using the market data.
     double computeTotalValue(const MarketData& marketData) const {
         double totalValue = cashBalance;
         for (const auto& [ticker, quantity] : assets) {
-            totalValue += quantity * marketData.getCurrentData(ticker).close;
+            if (marketData.getCurrentData(ticker).close != 0) { // Check for non-zero close price
+                totalValue += quantity * marketData.getCurrentData(ticker).close;
+            }
         }
         return totalValue;
     }
@@ -152,11 +195,27 @@ public:
 
     // Method to update the state based on new market data.
     void updateState(const MarketData& newMarketData) {
-        currentMarketData = newMarketData;
-        // ... any other state-related updates, e.g., updating historical performance ...
+        currentMarketData.next();
     }
 
-    // ... any other relevant methods for maintaining or updating the state ...
+    tensorflow::Tensor toTensor() const {
+        // Convert state to Tensor representation
+        // For this example, we'll just use historical data of a single ticker
+        auto historicalData = currentMarketData.getHistoricalData("TICKER", 10); // last 10 days
+        tensorflow::Tensor tensor(tensorflow::DT_FLOAT, {1, 60}); // 10 days * 6 data points
+        auto tensor_mapped = tensor.tensor<float, 2>();
+        int idx = 0;
+        for (const auto& dataPoint : historicalData) {
+            tensor_mapped(0, idx++) = dataPoint.open;
+            tensor_mapped(0, idx++) = dataPoint.close;
+            tensor_mapped(0, idx++) = dataPoint.high;
+            tensor_mapped(0, idx++) = dataPoint.low;
+            tensor_mapped(0, idx++) = dataPoint.volume;
+            tensor_mapped(0, idx++) = dataPoint.adjustedClose;
+        }
+        return tensor;
+    }
+
 };
 
 class Action {
@@ -193,6 +252,18 @@ public:
     }
 };
 
+class Experience {
+public:
+    State state;
+    Action action;
+    double reward;
+    State nextState;
+    bool done;
+
+    Experience(const State& s, const Action& a, double r, const State& ns, bool d)
+        : state(s), action(a), reward(r), nextState(ns), done(d) {}
+};
+
 
 class TradingEnvironment {
 private:
@@ -206,12 +277,6 @@ public:
     TradingEnvironment(const State& startingState, double startingBalance)
         : currentState(startingState), initialBalance(startingBalance), currentDay(0) {}
 
-    // Reset the environment to its initial state.
-    State reset() {
-        currentState = State(Portfolio(initialBalance), MarketData());
-        currentDay = 0;
-        return currentState;
-    }
 
     // Execute an action in the environment.
     std::tuple<State, double, bool> step(const Action& action) {
@@ -234,7 +299,7 @@ public:
 
         // Update market data for the next day (for simplicity, we'll assume the market data is updated elsewhere).
         currentDay++;
-        currentState.updateState(/* ... new market data ... */);
+        currentState.updateState();
 
         double newPortfolioValue = currentState.getPortfolio().totalValue(currentState.getMarketData());
 
@@ -247,6 +312,20 @@ public:
         return std::make_tuple(currentState, reward, done);
     }
 
+    // Reset the environment to its initial state.
+    State reset() {
+        currentState = State(Portfolio(initialBalance), MarketData({{"TICKER", {}}}));
+        currentDay = 0;
+        return currentState;
+    }
+    int stateSize() const {
+        return 60;
+    }
+
+    int actionSize() const {
+        return 3;
+    }
+  
 };
 
 
@@ -261,10 +340,13 @@ private:
     tensorflow::Output targetQValues; // Placeholder for target Q-values.
     tensorflow::Output loss;          // Loss function.
     tensorflow::Operation trainOp;    // Training operation.
+    tensorflow::Operation initOp;
 
     double epsilon;  // Exploration rate.
-
-    // ... other necessary members ...
+    const double discountFactor = 0.95; // Define hyperparameters as needed.
+    std::deque<Experience> memory; // Experience replay memory
+    const size_t memoryCapacity = 10000;
+    const size_t batchSize = 64;
 
 public:
     DQNAgent(int inputSize, int outputSize) 
@@ -291,13 +373,24 @@ public:
             {0, 0, 0, 0}, 
             0.001, 0.9, 0.999, 1e-8, loss);
 
+        // Define the experience replay memory
+        memory = std::deque<Experience>();
+
+        // Global variables initializer for Tensorflow
+        initOp = tensorflow::ops::GlobalVariablesInitializer(scope);
+        session.Run({}, {}, {initOp});
+
         // Initialize epsilon for exploration.
         epsilon = 1.0;
     }
 
     // Select an action based on the current state.
     Action selectAction(const State& state) {
-        if (/* random number */ < epsilon) {
+        std::uniform_real_distribution<double> distribution(0.0, 1.0);
+        std::random_device rd;
+        std::mt19937 generator(rd());
+
+        if (distribution(generator) < epsilon) {
             // Explore: select a random action.
             return Action(/* random action */);
         } else {
@@ -340,14 +433,68 @@ public:
         // Gradually reduce epsilon over time.
         epsilon *= 0.995;
     }
+    void remember(const State& state, const Action& action, double reward, const State& nextState, bool done) {
+        if (memory.size() == memoryCapacity) {
+            memory.pop_front();
+        }
+        memory.push_back(Experience(state, action, reward, nextState, done));
+    }
 
+    Action act(const State& state) {
+        if (static_cast<float>(rand()) / RAND_MAX < epsilon) {
+            // Explore: Return a random action
+            // For simplicity, let's assume there are three actions: BUY, SELL, and HOLD for a single ticker.
+            // You can expand this to handle multiple tickers or more complex actions.
+            int randomAction = rand() % 3;
+            switch(randomAction) {
+                case 0:
+                    return Action(Action::BUY, "TICKER", rand() % 100 + 1);  // Random quantity between 1 and 100
+                case 1:
+                    return Action(Action::SELL, "TICKER", rand() % 100 + 1); // Random quantity between 1 and 100
+                default:
+                    return Action(Action::HOLD, "TICKER", 0);
+            }
+        } else {
+            // Exploit: Return the action with the highest Q-value based on the neural network
+            std::vector<tensorflow::Tensor> outputs;
+            TF_CHECK_OK(session.Run({{modelInput, state.toTensor()}}, {qValues}, &outputs));
+            
+            // Assume Q-values tensor is 1D. Find the index of the maximum Q-value which corresponds to the best action.
+            auto q_values_t = outputs[0].flat<float>();
+            int bestActionIndex = std::distance(&q_values_t(0), std::max_element(&q_values_t(0), &q_values_t(0) + q_values_t.size()));
+            
+            // Convert bestActionIndex to an actual Action object. For simplicity, we'll consider only 3 actions like before.
+            switch(bestActionIndex) {
+                case 0:
+                    return Action(Action::BUY, "TICKER", rand() % 100 + 1);  // Random quantity for demonstration
+                case 1:
+                    return Action(Action::SELL, "TICKER", rand() % 100 + 1); // Random quantity for demonstration
+                default:
+                    return Action(Action::HOLD, "TICKER", 0);
+            }
+        }
+    }
+    size_t memorySize() const {
+        return memory.size();
+    }
 
-    // ... other methods, like saving/loading the model, etc. ...
+    size_t batchSize() const {
+        return batchSize;
+    }
+    std::vector<Experience> sample() {
+        std::vector<Experience> batch;
+        std::sample(memory.begin(), memory.end(), std::back_inserter(batch), batchSize, std::mt19937{std::random_device{}()});
+        return batch;
+    }
+    void saveModel(const std::string& path) {
+        // Placeholder: Save the model weights to the specified path
+        // You would implement this using TensorFlow's save functionality
+    }    
 };
 
 int main() {
     // Initialize environment and agent
-    TradingEnvironment env;
+    TradingEnvironment env(State(Portfolio(1000.0), MarketData({{"TICKER", {}}}));
     DQNAgent agent(env.stateSize(), env.actionSize());
 
     int numberOfEpisodes = 1000;
